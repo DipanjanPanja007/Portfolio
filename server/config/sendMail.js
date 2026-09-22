837b9e3a07eff7fba50214179e55e52fa1a68dc2
Dipanjan Panja <131897890+DipanjanPanja007@users.noreply.github.com>
2026-09-22 16:22:25 +0530

    Change SMTP port from 465 to 587

--- server/config/sendMail.js
+++ server/config/sendMail.js
@@ -6,7 +6,7 @@ dotenv.config();
 const sendMail = async ({ email, subject, message }) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
+    port: 587,
     secure: true,
     auth: {
       user: process.env.EMAIL_USER,
