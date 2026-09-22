bd360623332ef6014bfadd9f0a2fd864f789140c
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2026-09-22 12:20:32 +0530

    mail bug

--- server/config/sendMail.js
+++ server/config/sendMail.js
@@ -5,7 +5,9 @@ dotenv.config();
 
 const sendMail = async ({ email, subject, message }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
